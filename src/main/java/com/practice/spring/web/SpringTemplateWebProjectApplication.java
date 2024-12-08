701b8c4627849ab021c1878f505c35613482ef12
cn162vr9i <cn162vr9i.tech@gmail.com>
2024-12-08 22:01:23 +0900

    1st upload

---
+++ src/main/java/com/practice/spring/web/SpringTemplateWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplateWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/spring/web/SpringTemplateWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplateWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
